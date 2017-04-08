f07121ff312421b9ea069d8bfe6892b8c7a31a5f
David <dpweberza@gmail.com>
2017-04-09 01:17:45 +0200

    rebuilding site Sun Apr  9 01:17:46 SAST 2017

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,19 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
-# Go To Public folder
-cd public
-# Add changes to git.
+# Push latest source code
 git add -A
-
-# Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
+git push origin master
 
-# Push source and build repos.
+# Push compiled site
+cd public
+git add -A
+git commit -m "$msg"
 git push origin master
 
 # Come Back
